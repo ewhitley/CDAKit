4ccb2c42dead29b7468642b8097f65c1e79e972c
Eric Whitley <ewhitley@mac.com>
2016-02-11 07:19:44 -0600

    Initial Commit

---
+++ CDAKit/CDAKit.h
@@ -0,0 +1,19 @@
+//
+//  CDAKit.h
+//  CDAKit
+//
+//  Created by Eric Whitley on 2/11/16.
+//  Copyright © 2016 Eric Whitley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CDAKit.
+FOUNDATION_EXPORT double CDAKitVersionNumber;
+
+//! Project version string for CDAKit.
+FOUNDATION_EXPORT const unsigned char CDAKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CDAKit/PublicHeader.h>
+
+
